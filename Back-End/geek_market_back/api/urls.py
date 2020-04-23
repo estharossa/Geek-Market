71cdf9a148dbd381ff117044c4608bab04228f1c
estharossa <danik.encore@gmail.com>
2020-04-23 22:10:00 +0600

    [added] installed apps to settings

--- Back-End/geek_market_back/api/urls.py
+++ Back-End/geek_market_back/api/urls.py
@@ -1,3 +1,10 @@
 from django.urls import path
+from api.views_fbv import category_list, category_detail, product_list, product_detail, category_product
+
 urlpatterns = [
+    path('categories/', category_list),
+    path('categories/<int:category_id>', category_detail),
+    path('products/', product_list),
+    path('products/<int:product_id>', product_detail),
+    path('categories/<int:pk>/products', category_product),
 ]
